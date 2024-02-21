92df669fdb41464e91c3dca8865e9e9f7f8634a8
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-21 20:17:31 -0300

    imobdev

--- app/view/_template_bottom.php
+++ app/view/_template_bottom.php
@@ -1,5 +1,22 @@
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
     <script src=" https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.16/dist/jquery.mask.min.js "></script>
+    
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/colreorder/1.5.6/js/dataTables.colReorder.min.js"></script>
+
+    <script src="https://raw.githubusercontent.com/zejuniortdr/jquery.meiomask.9digito/master/jquery.meio.mask.min.js"></script> <script src="https://raw.githubusercontent.com/zejuniortdr/jquery.meiomask.9digito/master/meio-mask-init.js"></script>
+    <script src="src/js/_global.js?<?= filemtime('src/js/_global.js') ?>"></script>
+    <script src="src/js/<?= $page ?>.js?<?= filemtime('src/js/' . $page . '.js') ?>"></script>
 
     </body>
 </html>
