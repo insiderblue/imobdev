e4daaf694e7cb91c6b7fa3b419e509cb5dfb2ed7
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-22 09:36:48 -0300

    imobdev

---
+++ app/view/painel-delete-properties.php
@@ -0,0 +1,23 @@
+<?php if(isset($_GET["confirm"])) : ?>
+
+<div class="card shadow-lg mt-4">
+    <h5 class="card-header bg-success text-light">
+        Todos os imóveis foram deletados com sucesso!
+    </h5>
+    <div class="card-body">
+        <a href="./?properties">Clique aqui</a> para voltar para a página inicial.
+    </div>
+</div>
+
+<?php else : ?>
+
+<div class="card shadow-lg mt-4">
+    <h5 class="card-header bg-warning text-dark">
+        Esta ação removerá todos os imóveis cadastrados. Não será possível recuperá-los!
+    </h5>
+    <div class="card-body">
+        <a href="./?dev-random-properties&confirm">Clique aqui</a> para confirmar.
+    </div>
+</div>
+
+<?php endif; ?>
