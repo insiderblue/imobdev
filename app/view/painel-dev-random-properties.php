9cc84fd698b09df3c4fdf3a14be9ad24c1c283df
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-22 08:22:07 -0300

    imobdev

--- app/view/painel-dev-random-properties.php
+++ app/view/painel-dev-random-properties.php
@@ -1,9 +1,23 @@
+<?php if(isset($_GET["confirm"])) : ?>
 
-<div class="card shadow-lg mt-4">
-    <h5 class="card-header bg-info text-light">
-        Esta ação preencherá a plataforma com o número máximo de imóveis permitidos, contendo informações aleatórias.
-    </h5>
-    <div class="card-body">
-        <a href="./?neighborhoods">Clique aqui</a> para realizar esta ação.
+    <div class="card shadow-lg mt-4">
+        <h5 class="card-header bg-success text-light">
+            Toda a capacidade de imóveis do site foi consumida com sucesso!
+        </h5>
+        <div class="card-body">
+            <a href="./?properties">Clique aqui</a> visualizar a lista de imóveis cadastrados.
+        </div>
     </div>
-</div>
+
+<?php else : ?>
+
+    <div class="card shadow-lg mt-4">
+        <h5 class="card-header bg-info text-light">
+            Esta ação preencherá a plataforma com o número máximo de imóveis permitidos, contendo informações aleatórias.
+        </h5>
+        <div class="card-body">
+            <a href="./?dev-random-properties&confirm">Clique aqui</a> para realizar esta ação.
+        </div>
+    </div>
+
+<?php endif; ?>
