434342580272881222ef6fc18a5f4fbb56117f71
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-22 09:44:31 -0300

    imobdev

--- app/view/painel-dev-random-properties.php
+++ app/view/painel-dev-random-properties.php
@@ -15,7 +15,7 @@
 
     <div class="card shadow-lg mt-4">
         <h5 class="card-header bg-warning text-dark">
-            Esta ação preencherá a plataforma com o número máximo de imóveis permitidos, contendo informações aleatórias.
+            Esta ação criará diversos imóveis contendo informações aleatórias. Utilize quantas vezes for necessário.
         </h5>
         <div class="card-body">
             <a href="./?dev-random-properties&confirm">Clique aqui</a> para confirmar.
