7ae17aa843f51d56868e032727b0f7d3720942b5
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-24 09:38:13 -0300

    imobdev

--- api/index.php
+++ api/index.php
@@ -443,8 +443,8 @@ if (isset($_REQUEST["fixed_properties"])) {
     INNER JOIN neighborhoods c ON a.neighborhood_id = c.neighborhood_id
     INNER JOIN property_types d ON a.property_type_id = d.property_type_id
     INNER JOIN property_goals e ON a.property_goal_id = e.property_goal_id
-        AND a.real_estate_id = e.real_estate_id
     WHERE a.real_estate_id = ' . $_REQUEST["real_estate_id"] . '
+    AND e.real_estate_id = ' . $_REQUEST["real_estate_id"] . '
     AND a.property_goal_id = ' . $_REQUEST["property_goal_id"] . '
     AND a.property_deleted = 0
     AND a.property_public = 1
