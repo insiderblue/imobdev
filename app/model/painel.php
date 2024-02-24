477def7f497aa92b911b89de77e1fde613842f8e
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-24 09:14:18 -0300

    imobdev

--- app/model/painel.php
+++ app/model/painel.php
@@ -1,12 +1,14 @@
 <?php
 
+
 $cache_clear = $pdo->prepare('UPDATE real_estates SET real_estate_clear_cache = :real_estate_clear_cache WHERE real_estate_id = :real_estate_id');
 $cache_clear->execute(
     array(
         'real_estate_clear_cache'   => 1,
-        'real_estate_id'            => $_REQUEST["real_estate_id"]
+        'real_estate_id'            => $_SESSION["real_estate_id"]
     )
 );
+
 /** New property */
 
 if ($_POST["new_property"] == true) {
