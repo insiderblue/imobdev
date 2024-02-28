f69350f088593df6d55333169da359eb34c74dc7
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-02-28 08:29:01 -0300

    imobdev

--- api/index.php
+++ api/index.php
@@ -221,7 +221,8 @@ if (isset($_REQUEST["property_goals"])) {
     a.property_goal_title,
     a.property_goal_title_secondary,
     a.property_goal_title_tertiary,
-    a.property_goal_title_quaternary
+    a.property_goal_title_quaternary,
+    a.property_goal_title_quinary
     FROM property_goals a
     INNER JOIN properties b ON a.property_goal_id = b.property_goal_id
     WHERE a.real_estate_id = ' . $_REQUEST["real_estate_id"] . '
@@ -245,7 +246,8 @@ if (isset($_REQUEST["property_goals_not_exclude"])) {
     a.property_goal_title,
     a.property_goal_title_secondary,
     a.property_goal_title_tertiary,
-    a.property_goal_title_quaternary
+    a.property_goal_title_quaternary,
+    a.property_goal_title_quinary
     FROM property_goals a
     WHERE a.real_estate_id = ' . $_REQUEST["real_estate_id"] . '
     ');
