45d974ce46c6bab560ee34ba5f8f2be6ed5fe268
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-03-05 11:53:29 -0300

    imobdev

--- app/view/_template_top.php
+++ app/view/_template_top.php
@@ -17,7 +17,7 @@
   <link href="src/css/_global.css?<?= filemtime('src/css/_global.css') ?>" rel="stylesheet">
   <link href="src/css/<?= $page ?>.css?<?= filemtime('src/css/' . $page . '.css') ?>" rel="stylesheet">
   
-    <title>Imobdev - <?= $real_estate_name ?></title>
+    <title>Imobdev - <?= $real_estate_name ?? 'Fazer login' ?></title>
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
