bfab633596371ad6eedd3f97efeaf73fb04917ee
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-03-05 11:51:07 -0300

    imobdev

--- app/view/_template_top.php
+++ app/view/_template_top.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+  <link rel="shortcut icon" href="<?= $real_estate_favicon ?>"/>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
@@ -13,8 +16,8 @@
 
   <link href="src/css/_global.css?<?= filemtime('src/css/_global.css') ?>" rel="stylesheet">
   <link href="src/css/<?= $page ?>.css?<?= filemtime('src/css/' . $page . '.css') ?>" rel="stylesheet">
-  <title>Imobdev</title>
-
+  
+    <title><?= $real_estate_name ?></title>
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
